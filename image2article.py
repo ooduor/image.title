e0c7575809dfd5b86986071347df75b0dde97269
antony oduor <anthony@bellwether>
2019-07-17 23:20:32 -0600

    Splits Titles and Contents Into Two Different Masks! Lit
    Signed-off-by: antony oduor <anthony@bellwether>

--- image2article.py
+++ image2article.py
@@ -16,37 +16,32 @@ cv2.imwrite('binary_gray.png', im_bw)
 
 # image_rlsa = rlsa.rlsa(im_bw, True, False, 10) # horizontal
 # image_rlsa = rlsa.rlsa(im_bw, False, True, 10) # vertical
-# image_rlsa = rlsa.rlsa(im_bw, True, True, 10) # both
+# image_rlsa = rlsa.rlsa(im_bw, True, True, 8) # both
 # cv2.imshow('binary_gray_horizontal', image_rlsa)
 # cv2.imwrite('binary_gray_horizontal.png', image_rlsa)
 
 mask = np.ones(image.shape[:2], dtype="uint8") * 255 # create blank image of same dimension of the original image
+mask_content = np.ones(image.shape[:2], dtype="uint8") * 255 # create blank image of same dimension of the original image
 (contours, _) = cv2.findContours(~im_bw, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 heights = [cv2.boundingRect(contour)[3] for contour in contours] # collecting heights of each contour
-
-for idx, contour in enumerate(contours):
-    """
-    draw a rectangle around those contours on main image
-    """
-    [x,y,w,h] = cv2.boundingRect(contour)
-    if idx < 5000:
-        cv2.rectangle(image, (x,y), (x+w,y+h), (255, 0, 0), 1)
-    else:
-        cv2.rectangle(image, (x,y), (x+w,y+h), (0, 255, 0), 1)
-
-cv2.imshow('contour', image)
-cv2.imwrite('contours.png', image)
-
+widths = [cv2.boundingRect(contour)[2] for contour in contours] # collecting heights of each contour
 avgheight = sum(heights)/len(heights) # average height
+avgwidth = sum(widths)/len(widths) # average width
 
 # finding the larger text
-for c in contours:
-    [x,y,w,h] = cv2.boundingRect(c)
+for idx, contour in enumerate(contours):
+    [x,y,w,h] = cv2.boundingRect(contour)
+    cv2.rectangle(image, (x,y), (x+w,y+h), (0, 255, 0), 1)
     if h > 2*avgheight:
-        cv2.drawContours(mask, [c], -1, 0, -1)
+        cv2.drawContours(mask, [contour], -1, 0, -1)
+    else:
+        cv2.drawContours(mask_content, [contour], -1, 0, -1) # everything else not heading-like
 
-cv2.imshow('mask', mask)
-cv2.imwrite('mask0.png', mask)
+cv2.imshow('contour', image) # on original image
+cv2.imwrite('contours.png', image)
+
+cv2.imshow('mask', mask_content) # on mask
+cv2.imwrite('mask_content.png', mask_content)
 
 x, y = mask.shape # image dimensions
 
@@ -54,8 +49,8 @@ value = max(math.ceil(x/100),math.ceil(y/100))+10
 value = 10
 mask = rlsa.rlsa(mask, True, False, value) #rlsa application
 
-cv2.imshow('mask1', mask)
-cv2.imwrite('mask1.png', mask)
+cv2.imshow('mask-fin', mask)
+cv2.imwrite('mask-fin.png', mask)
 
 (contours, _) = cv2.findContours(~mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
