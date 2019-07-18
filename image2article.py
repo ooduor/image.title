5d5c11b805cc430ff3d9d0c4c37ad2e1eb8835ff
Antony Owino <owino@ualberta.ca>
2019-07-18 10:42:10 -0600

    Another Milestone, Chunking Big Text Blocks
    Next step, Algorithm to Traverse Top-Down using Avg Width as
    Delimited for Column by Column Based on Width of the Document.
    
    Signed-off-by: Antony Owino <owino@ualberta.ca>

--- image2article.py
+++ image2article.py
@@ -22,6 +22,7 @@ cv2.imwrite('binary_gray.png', im_bw)
 
 mask = np.ones(image.shape[:2], dtype="uint8") * 255 # create blank image of same dimension of the original image
 mask_content = np.ones(image.shape[:2], dtype="uint8") * 255 # create blank image of same dimension of the original image
+mask_content2 = np.ones(image.shape, dtype="uint8") * 255 # blank 3 layer image
 (contours, _) = cv2.findContours(~im_bw, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 heights = [cv2.boundingRect(contour)[3] for contour in contours] # collecting heights of each contour
 widths = [cv2.boundingRect(contour)[2] for contour in contours] # collecting heights of each contour
@@ -31,14 +32,32 @@ avgwidth = sum(widths)/len(widths) # average width
 # finding the larger text
 for idx, contour in enumerate(contours):
     [x,y,w,h] = cv2.boundingRect(contour)
-    cv2.rectangle(image, (x,y), (x+w,y+h), (0, 255, 0), 1)
+    # cv2.rectangle(image, (x,y), (x+w,y+h), (0, 255, 0), 1)
     if h > 2*avgheight:
-        cv2.drawContours(mask, [contour], -1, 0, -1)
+        cv2.drawContours(mask, [contour], -1, 0, -1) # heading like contours
     else:
         cv2.drawContours(mask_content, [contour], -1, 0, -1) # everything else not heading-like
 
 cv2.imshow('contour', image) # on original image
 cv2.imwrite('contours.png', image)
+
+# attempt to get large content blocks
+image_rlsa = rlsa.rlsa(mask_content, True, True, 10) # both hori and verti
+(contours, _) = cv2.findContours(~image_rlsa, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+# finding the huge context blocks
+for idx, contour in enumerate(contours):
+    [x,y,w,h] = cv2.boundingRect(contour)
+    cv2.rectangle(image, (x,y), (x+w,y+h), (0, 0, 255), 3)
+    cv2.drawContours(mask_content2, [contour], -1, 0, -1) # heading like contours
+    contents = image[y: y+h, x: x+w]
+    mask_content2[y: y+h, x: x+w] = contents # copied contents contour onto the blank image
+    image[y: y+h, x: x+w] = 255 # nullified the title contour on original image
+
+cv2.imshow('contours_content', mask_content2) # on original image
+cv2.imwrite('contours_content.png', mask_content2)
+
+cv2.imshow('binary_gray_horizontal', image_rlsa)
+cv2.imwrite('binary_gray_horizontal.png', image_rlsa)
 
 cv2.imshow('mask', mask_content) # on mask
 cv2.imwrite('mask_content.png', mask_content)
@@ -54,7 +73,7 @@ cv2.imwrite('mask-fin.png', mask)
 
 (contours, _) = cv2.findContours(~mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-mask2 = np.ones(image.shape, dtype="uint8") * 255 # blank 3 layer image
+mask2 = np.ones(image.shape, dtype="uint8") * 255 # blank 4 layer image
 for contour in contours:
     [x, y, w, h] = cv2.boundingRect(contour)
     if w > 0.60*image.shape[1]:# width heuristic applied
